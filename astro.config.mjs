76dc2db469730fc1f297b276d31c3fe662431d32
jrap <jonathanrapaport@gmail.com>
2025-12-23 11:38:30 -0500

    SEO updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://reluctantwinner.com',
+  output: 'static'
 });
